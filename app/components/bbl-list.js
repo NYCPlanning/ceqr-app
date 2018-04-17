7c9151610c8d28548595569bd3d0cd2203935c4c
Jonathan Pichot <jonathan@pichot.us>
2018-04-17 18:08:17 -0400

    bbl list adds multiple

--- app/components/bbl-list.js
+++ app/components/bbl-list.js
@@ -4,6 +4,10 @@ export default Component.extend({
   actions: {
     addBbl(bbl) {
       this.model.get('bbls').pushObject(bbl);
-    }
+      this.set('bbl', '');
+    },
+    removeBbl(bbl) {
+      this.model.get('bbls').removeObject(bbl);
+    },
   }
 });
