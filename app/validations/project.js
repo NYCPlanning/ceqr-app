ea39c9748d986f1f3f1ff6fc1e1ad0ec1b767a0f
Jonathan Pichot <jonathan@pichot.us>
2018-08-07 13:58:24 -0400

    remove validation for ceqr number

--- app/validations/project.js
+++ app/validations/project.js
@@ -2,7 +2,6 @@ import { validatePresence, validateNumber, validateLength } from 'ember-changese
 
 export default {
   name: validateLength({ min: 3 }),
-  ceqr_number: validatePresence(true),
   buildYear: validatePresence(true),
   totalUnits: validateNumber({gte: 1}),
   bbls: validateLength({ min: 1, message: 'The project should have at least one BBL'})
