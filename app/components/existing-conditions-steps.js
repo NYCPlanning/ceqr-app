c3b58b8320045a39b9c778519e907934db450560
Jonathan Pichot <jonathan@pichot.us>
2018-08-23 13:28:18 -0400

    Define study area notice

--- app/components/existing-conditions-steps.js
+++ app/components/existing-conditions-steps.js
@@ -1,10 +1,17 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   init() {
     this._super(...arguments)
     this.set('tab', 'schools');
   },
+
+  studyAreaNotice: computed('project.{esSchoolChoice,isSchoolChoice,subdistrictsFromDB.[]}', function() {
+    return (this.get('esSchoolChoice') || this.get('esSchoolChoice') || 
+      (this.get('project.subdistrictsFromDB').length > 1)
+    );
+  }),
 
   actions: {
     setTab(tab) {
