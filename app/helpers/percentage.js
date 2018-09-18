9aa81b82c5ca4d09e59a870b1d3841a994b2988c
Jonathan Pichot <jonathan@pichot.us>
2018-09-18 14:18:30 -0400

    improve percentage

--- app/helpers/percentage.js
+++ app/helpers/percentage.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
-export function percentage([value, ...rest]) {
-  let percent = value * 100;
+export function percentage([value]) {
+  let percent = Math.round(value * 100);
 
   return `${percent}%`;
 }
