a6eeb56b97dbdc6c6c2a2ca03276725d90d23350
Jonathan Pichot <jonathan@pichot.us>
2018-10-17 14:02:34 -0400

    school choice works

--- app/components/existing-conditions-steps.js
+++ app/components/existing-conditions-steps.js
@@ -1,10 +1,4 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
-  studyAreaNotice: computed('project.{esSchoolChoice,isSchoolChoice,subdistrictsFromDB.[]}', function() {
-    return (this.get('esSchoolChoice') || this.get('esSchoolChoice') || 
-      (this.get('project.subdistrictsFromDb').length > 1)
-    );
-  }),
 });
