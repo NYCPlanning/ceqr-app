5e266b7b1132cdae9e17504b7d96eb20616c1482
Jonathan Pichot <jonathan@pichot.us>
2018-11-01 15:17:08 -0400

    fix percentage issue

--- app/decorators/Building.js
+++ app/decorators/Building.js
@@ -4,18 +4,16 @@ import round from '../utils/round';
 
 export default EmberObject.extend({      
   seats: computed('excluded', 'enroll', 'capacity', function() {
-    if (this.get('excluded')) return Math.round(0 - this.get('enroll'));
-    if (!this.get('capacity')) return 0;
-    return Math.round(this.get('capacity') - this.get('enroll'));
+    if (this.excluded) return Math.round(0 - this.enroll);
+    if (!this.capacity) return 0;
+    return Math.round(this.capacity - this.enroll);
   }),
   
   utilization: computed('excluded', 'enroll', 'capacity', function() {
-    if (this.get('excluded')) return null;
-    if (!this.get('capacity')) return 0;
-    return round((this.get('enroll') / this.get('capacity')), 3);
+    return round((this.enroll / this.capacity), 3);
   }),
 
   capacityDelta: computed('capacity', 'capacityFuture', function() {
-    return this.get('capacityFuture') - this.get('capacity');
+    return this.capacityFuture - this.capacity;
   })
 });
--- app/helpers/percentage.js
+++ app/helpers/percentage.js
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 import round from '../utils/round';
 
 export function percentage([value], { decimals = 0 }) {
-  return `${round(value, decimals)}%`;
+  return `${round((value * 100), decimals)}%`;
 }
 
 export default helper(percentage);
