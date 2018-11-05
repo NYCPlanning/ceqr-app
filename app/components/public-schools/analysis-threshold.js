8e207d4b4bfd4ce51ebbaa0c1265b7c93e47c9da
Jonathan Pichot <jonathan@pichot.us>
2018-11-05 13:46:50 -0500

    finished new multipliers

--- app/components/public-schools/analysis-threshold.js
+++ app/components/public-schools/analysis-threshold.js
@@ -1,9 +1,14 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import $ from 'jquery';
 
 export default Component.extend({
   'schools-capacity': service(),
   store: service(),
+
+  didRender() {
+    $('.dropdown').dropdown();
+  },
 
   actions: {
     async changeManualVersion(version) {
--- app/components/public-schools/analysis-threshold/nov-2018-multipliers.js
+++ app/components/public-schools/analysis-threshold/nov-2018-multipliers.js
@@ -1,4 +1,25 @@
 import Component from '@ember/component';
+import { computed } from '@ember-decorators/object';
 
-export default Component.extend({
-});
+export default class Nov2018Multipliers extends Component {
+  
+  @computed('project.manual.districts')
+  get multipliers() {
+    const districts = this.project.manual.districts;
+    if (!districts) return {};
+    
+    const bx = districts.filterBy('borocode', 'bx').sortBy('csd');
+    const bk = districts.filterBy('borocode', 'bk').sortBy('csd');
+    const mn = districts.filterBy('borocode', 'mn').sortBy('csd');
+    const qn = districts.filterBy('borocode', 'qn').sortBy('csd');
+    const si = districts.filterBy('borocode', 'si').sortBy('csd');
+
+    return {
+      bx: { count: bx.length, districts: bx },
+      bk: { count: bk.length, districts: bk },
+      mn: { count: mn.length, districts: mn },
+      qn: { count: qn.length, districts: qn },
+      si: { count: si.length, districts: si },
+    };
+  } 
+}
