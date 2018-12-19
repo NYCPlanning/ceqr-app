8bb4697e3c74aed722dc64d0f9499fe8f3a46247
Jonathan Pichot <jonathan@pichot.us>
2018-12-19 17:06:49 -0500

    data and multipliers connected

--- frontend/app/components/public-schools/version-selector.js
+++ frontend/app/components/public-schools/version-selector.js
@@ -7,10 +7,18 @@ export default Component.extend({
   
   actions: {
     async changeDataVersion(version) {
+      const dataMultiplierMap = {
+        "nov17": "march-2014",
+        "nov18": "november-2018"
+      }
+      
       const analysis = await this.analysis;
       const dataTables = await this.store.findRecord('data-tables/public-schools', version);
+      const multipliers = await this.store.findRecord('ceqr-manual/public-schools', dataMultiplierMap[version]);
 
       analysis.set('dataTables', dataTables);
+      analysis.set('multipliers', multipliers);
+
       await analysis.save();
 
       this.get('public-schools').set('analysis', analysis);
