6e49591686daecf9212e79edfd6878d214c25fd5
Jonathan Pichot <jonathan@pichot.us>
2019-01-08 13:46:13 -0500

    upgrade babel

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -5,7 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     babel: {
-      plugins: ['transform-object-rest-spread']
+      plugins: [
+        "@babel/plugin-proposal-object-rest-spread"
+      ]
     }
   });
 
