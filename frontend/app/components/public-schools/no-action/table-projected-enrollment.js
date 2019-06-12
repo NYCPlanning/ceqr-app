ec670cbe0cec1e67a43bbc3d85eb93e3bbbba135
Jonathan Pichot <jonathan@pichot.us>
2019-06-12 15:27:07 -0400

    Consolidate LevelTotals on public-schools-analysis model
    Every public-schools-analysis model now has one LevelTotals per level, accessible by all relevant components.

--- frontend/app/components/public-schools/no-action/table-projected-enrollment.js
+++ frontend/app/components/public-schools/no-action/table-projected-enrollment.js
@@ -1,35 +1,4 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import LevelTotals from '../../../fragments/public-schools/LevelTotals';
 
 export default Component.extend({
-  psTable: computed('analysis.subdistrictTotals.@each', function() {
-    return this.analysis.subdistrictTotals.filterBy('level', 'ps');
-  }),
-
-  psTableTotals: computed('analysis.subdistrictTotals.@each', function() {
-    return LevelTotals.create({
-      subdistrictTotals: this.analysis.subdistrictTotals.filterBy('level', 'ps')
-    });
-  }),
-
-  isTable: computed('analysis.subdistrictTotals.@each', function() {
-    return this.analysis.subdistrictTotals.filterBy('level', 'is');
-  }),
-
-  isTableTotals: computed('analysis.subdistrictTotals.@each', function() {
-    return LevelTotals.create({
-      subdistrictTotals: this.analysis.subdistrictTotals.filterBy('level', 'is')
-    });
-  }),
-
-  hsTable: computed('analysis.subdistrictTotals.@each', function() {
-    return this.analysis.subdistrictTotals.filterBy('level', 'hs');
-  }),
-
-  hsTableTotals: computed('analysis.subdistrictTotals.@each', function() {
-    return LevelTotals.create({
-      subdistrictTotals: this.analysis.subdistrictTotals.filterBy('level', 'hs')
-    });
-  }),
 });
