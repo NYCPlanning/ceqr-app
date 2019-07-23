b484ce17fafaa3a91e7d3d5d8867eac16543f4f5
Jonathan Pichot <jonathan@pichot.us>
2019-07-23 12:44:35 -0400

    Fix JTW study area selections to be correct postgres array type

--- backend/db/migrate/20190610181704_add_required_jtw_study_selection_to_transportation_analysis.rb
+++ backend/db/migrate/20190610181704_add_required_jtw_study_selection_to_transportation_analysis.rb
@@ -1,6 +1,6 @@
 class AddRequiredJtwStudySelectionToTransportationAnalysis < ActiveRecord::Migration[5.2]
   def change
-    add_column :transportation_analyses, :required_jtw_study_selection, 'varchar(11)', default: [], array: true
+    add_column :transportation_analyses, :required_jtw_study_selection, :text, array: true, default: []
     TransportationAnalysis.all.each do |a|
       a.send(:compute_required_study_selection)
       a.save!
@@ -9,6 +9,6 @@ class AddRequiredJtwStudySelectionToTransportationAnalysis < ActiveRecord::Migra
 
     # also, recreate the jtw_study_selection column as varchar(11)[] type
     remove_column :transportation_analyses, :jtw_study_selection
-    add_column :transportation_analyses, :jtw_study_selection, 'varchar(11)', default: [], array: true
+    add_column :transportation_analyses, :jtw_study_selection, :text, array: true, default: []
   end
 end
