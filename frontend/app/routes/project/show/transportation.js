e68d688fdf29abce145cf0d2e187dfe4818158be
godfreyyeung <godfreyyeung@gmail.com>
2019-07-23 17:10:44 -0400

    ensure model is loaded in analysis-threshold route

--- frontend/app/routes/project/show/transportation.js
+++ frontend/app/routes/project/show/transportation.js
@@ -2,6 +2,10 @@ import Route from '@ember/routing/route';
 
 export default class ProjectShowTransportationRoute extends Route {
 
+  redirect(model) {
+    this.transitionTo('project.show.transportation.analysis-threshold', model);
+  }
+
   async setupController(controller) {
     const { project } = this.modelFor('project/show');
     const transportationAnalysis = await project.get('transportationAnalysis');
