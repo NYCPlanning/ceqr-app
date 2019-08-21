75bce91acd07ccbeed84f1378787a1ed6f9664b8
Jonathan Pichot <jonathan@pichot.us>
2019-08-21 14:20:16 -0400

    Schema migration bluebook, lcgms, sca_projects

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_172311) do
+ActiveRecord::Schema.define(version: 2019_08_21_153514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
