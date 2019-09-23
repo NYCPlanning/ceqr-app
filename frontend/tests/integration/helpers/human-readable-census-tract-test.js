003eaae8e33dcc9f85d5531317c32d6e5d68184d
Jonathan Pichot <jonathan@pichot.us>
2019-09-23 14:05:20 -0400

    fix tests

--- frontend/tests/integration/helpers/human-readable-census-tract-test.js
+++ frontend/tests/integration/helpers/human-readable-census-tract-test.js
@@ -15,26 +15,26 @@ module('Integration | Helper | human-readable-census-tract', function(hooks) {
     // Bronx County 
     this.set('bronxGeoid', `${state}005${censusTract}`);
     await render(hbs`{{human-readable-census-tract bronxGeoid}}`);
-    assert.equal(this.element.textContent.trim(), `Census Tract ${censusTract}, Bronx County, New York`);
+    assert.equal(this.element.textContent.trim(), `${censusTract}, Bronx County`);
 
     // Queen's County 
     this.set('queensGeoid', `${state}047${censusTract}`);
     await render(hbs`{{human-readable-census-tract queensGeoid}}`);
-    assert.equal(this.element.textContent.trim(), `Census Tract ${censusTract}, Queen's County, New York`);
+    assert.equal(this.element.textContent.trim(), `${censusTract}, Queen's County`);
 
     // New York County 
     this.set('newYorkGeoid', `${state}061${censusTract}`);
     await render(hbs`{{human-readable-census-tract newYorkGeoid}}`);
-    assert.equal(this.element.textContent.trim(), `Census Tract ${censusTract}, New York County, New York`);
+    assert.equal(this.element.textContent.trim(), `${censusTract}, New York County`);
 
     // King's County
     this.set('kingsGeoid', `${state}081${censusTract}`);
     await render(hbs`{{human-readable-census-tract kingsGeoid}}`);
-    assert.equal(this.element.textContent.trim(), `Census Tract ${censusTract}, King's County, New York`);
+    assert.equal(this.element.textContent.trim(), `${censusTract}, King's County`);
 
     // Richmond County
     this.set('richmondGeoid', `${state}085${censusTract}`);
     await render(hbs`{{human-readable-census-tract richmondGeoid}}`);
-    assert.equal(this.element.textContent.trim(), `Census Tract ${censusTract}, Richmond County, New York`);
+    assert.equal(this.element.textContent.trim(), `${censusTract}, Richmond County`);
   });
 });
