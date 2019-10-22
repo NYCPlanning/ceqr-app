d410e70f28f5321ea9522bb106a7823d5f30d286
Jonathan Pichot <jonathan@pichot.us>
2019-10-22 17:10:18 -0400

    fix migrations

--- backend/db/migrate/20190909213144_migrate_transportation_analysis_to_data_package.rb
+++ backend/db/migrate/20190909213144_migrate_transportation_analysis_to_data_package.rb
@@ -1,8 +1,8 @@
 class MigrateTransportationAnalysisToDataPackage < ActiveRecord::Migration[5.2]
   def change
     TransportationAnalysis.all.each do |ta|
-      ta.nyc_acs_data_package = DataPackage.latest_for("nyc_acs")
-      ta.ctpp_data_package = DataPackage.latest_for("ctpp")
+      ta.nyc_acs_data_package_id = DataPackage.latest_for("nyc_acs").id
+      ta.ctpp_data_package_id = DataPackage.latest_for("ctpp").id
 
       ta.save
     end
