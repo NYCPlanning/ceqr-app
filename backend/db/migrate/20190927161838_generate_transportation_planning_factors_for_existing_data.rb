4dc4c8f3454fd942425ed4c960d14ba628a8d851
Jonathan Pichot <jonathan@pichot.us>
2019-10-30 13:07:00 -0400

    Fix existing data migration

--- backend/db/migrate/20190927161838_generate_transportation_planning_factors_for_existing_data.rb
+++
@@ -1,5 +0,0 @@
-class GenerateTransportationPlanningFactorsForExistingData < ActiveRecord::Migration[5.2]
-  def change
-    TransportationAnalysis.all.each &:compute_for_changed_land_use!
-  end
-end
