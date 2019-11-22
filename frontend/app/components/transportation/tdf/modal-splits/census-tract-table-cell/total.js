a51cb96c42dc1de6b31041704b18c80eae9c0e76
Jonathan Pichot <jonathan@pichot.us>
2019-11-22 14:57:51 -0500

    Modal Split selection change

---
+++ frontend/app/components/transportation/tdf/modal-splits/census-tract-table-cell/total.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+import { computed } from '@ember-decorators/object';
+import { censusTractVariableForMode } from 'labs-ceqr/utils/censusTractVariableForMode';
+
+export default class TransportationTdfModalSplitsCensusTractTableCellTotalComponent extends Component {
+  tagName = 'td';
+
+  @computed('activeModes', 'tract')
+  get tractTotal() {
+    let total = 0;
+    
+    this.activeModes.forEach((mode) => {
+      const key = censusTractVariableForMode(mode);
+      total += this.tract[key].value;
+    });
+
+    return total;
+  }
+}
