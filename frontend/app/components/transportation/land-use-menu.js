c719ed5b34c5b2e8c6594e6fe2feb28079da488e
trbmcginnis <taylormcgin@gmail.com>
2019-12-19 17:09:12 -0500

    update

--- frontend/app/components/transportation/land-use-menu.js
+++ frontend/app/components/transportation/land-use-menu.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember-decorators/object';
+import { computed, action } from '@ember-decorators/object';
 
 export default class TransportationLandUseMenuComponent extends Component {
   showTotalLink = false;
@@ -12,5 +12,10 @@ export default class TransportationLandUseMenuComponent extends Component {
   @computed('tdfSection')
   get totalLink() {
     return `project.show.transportation.tdf.${this.tdfSection}.total`;
+  }
+
+  @action
+  saveFactors() {
+    this.model.transportationPlanningFactors.save();
   }
 }
