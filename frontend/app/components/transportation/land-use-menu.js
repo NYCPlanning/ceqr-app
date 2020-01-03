1a600fb60b213e73c7baca2d7e3fd7a3b52c1d29
trbmcginnis <taylormcgin@gmail.com>
2020-01-02 21:46:28 -0500

    update

--- frontend/app/components/transportation/land-use-menu.js
+++ frontend/app/components/transportation/land-use-menu.js
@@ -1,8 +1,17 @@
 import Component from '@ember/component';
 import { computed, action } from '@ember-decorators/object';
+import { inject as service } from '@ember-decorators/service';
 
 export default class TransportationLandUseMenuComponent extends Component {
+  @service
+  notificationMessages;
+
+  @service
+  flashMessages;
+
   showTotalLink = false;
+
+  showMessage = false;
 
   @computed('tdfSection')
   get factorLink() {
@@ -17,5 +26,11 @@ export default class TransportationLandUseMenuComponent extends Component {
   @action
    saveFactors() {
      this.model.transportationPlanningFactors.save();
+     this.get('flashMessages').success('Success!');
+     this.get('notificationMessages').success('Project Saved!', {
+       autoClear: true,
+       clearDuration: 1500,
+       cssClasses: 'chocolate'
+     });
   }
 }
