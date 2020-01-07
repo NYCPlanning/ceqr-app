20ef4330c2e5fbb35ab33c2859feb8221e32605f
godfreyyeung <godfreyyeung@gmail.com>
2020-01-07 11:54:13 -0500

    Replace old jtw* variable names w censusTracts*

--- frontend/mirage/factories/transportation-analysis.js
+++ frontend/mirage/factories/transportation-analysis.js
@@ -2,13 +2,13 @@ import { Factory, association } from 'ember-cli-mirage';
 
 export default Factory.extend({
   trafficZone: 2,
-  requiredJtwStudySelection: () => [
+  requiredCensusTractsSelection: () => [
       "36061020300"
   ],
-  jtwStudySelection: () => [
+  censusTractsSelection: () => [
     '36061020500', '36061021100', '36061019701', '36061020701', '36061020101', '36061019900'
   ],
-  jtwStudyAreaCentroid: () => [
+  censusTractsCentroid: () => [
     -73.964251, 40.8080809
   ],
   project: association({
