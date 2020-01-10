83989eaa2876ab83d1fa9e561bcebe9a932e47b1
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-01-10 16:37:35 -0500

    537:  Add lint rule modifications
    This commit adds all the rules from zola,
    except the ember rules.  The ember rule comes from
    zap-search.
    
    With these rules, the linter gives us:
    ✖ 13333 problems (13333 errors, 0 warnings)
      13161 errors and 0 warnings potentially fixable with the `--fix`

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,6 +20,19 @@ module.exports = {
     browser: true
   },
   rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'lines-around-directive': 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'prefer-arrow-callback': 0,
+    'no-underscore-dangle': 0,
+    'camelcase': 0,
+    'class-methods-use-this': 0,
+    'max-len': 0,
+    'no-param-reassign': 0,
+    'ember/avoid-leaking-state-in-ember-objects': 0
   },
   overrides: [
     // node files
