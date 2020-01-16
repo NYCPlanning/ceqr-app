baeee5a95da9826e2150c06130278e582c994887
trbmcginnis <taylormcgin@gmail.com>
2020-01-16 13:36:06 -0500

    564: add test for user manual input of temporal mode split values
    When a user is on the Manual tab and the Temporal Splits tab, they
    can enter mode split values for each time of day (am, md, pm, saturday)
    AND for each mode (e.g. auto, taxi, bus). The totals that are
    calculated through the `total` computed property in modal-splits.js
    produces the correct total value.

--- frontend/tests/integration/components/transportation/tdf/modal-splits-test.js
+++ frontend/tests/integration/components/transportation/tdf/modal-splits-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
@@ -57,6 +57,33 @@ module('Integration | Component | transportation/tdf/modal-splits', function(hoo
       {{/transportation/tdf/modal-splits}}
     `);
 
-    assert.ok(this.element);
+    // if manualModeSplits is true, then modeSplits = modeSplitsFromUser, which are all default 0
+    assert.equal(this.element.querySelector('[data-test-total-am]').textContent, '0', 'total am default');
+    assert.equal(this.element.querySelector('[data-test-total-md]').textContent, '0', 'total md default');
+    assert.equal(this.element.querySelector('[data-test-total-pm]').textContent, '0', 'total pm default');
+    assert.equal(this.element.querySelector('[data-test-total-saturday]').textContent, '0', 'total saturday default');
+
+    // USER FILLS IN MANUAL MODE SPLITS
+    // total for am should be 3
+    await fillIn('[data-test-modal-split-input-am="auto"]', 1);
+    await fillIn('[data-test-modal-split-input-am="taxi"]', 2);
+
+    // total for md should be 7
+    await fillIn('[data-test-modal-split-input-md="bus"]', 3);
+    await fillIn('[data-test-modal-split-input-md="subway"]', 4);
+
+    // total for pm should be 11
+    await fillIn('[data-test-modal-split-input-pm="railroad"]', 5);
+    await fillIn('[data-test-modal-split-input-pm="auto"]', 6);
+
+    // total for sat should be 15
+    await fillIn('[data-test-modal-split-input-saturday="taxi"]', 7);
+    await fillIn('[data-test-modal-split-input-saturday="bus"]', 8);
+
+    // tests for total.am, total.md, total.pm, total.saturday in computed property `total`
+    assert.equal(this.element.querySelector('[data-test-total-am]').textContent, '3', 'total am calculated');
+    assert.equal(this.element.querySelector('[data-test-total-md]').textContent, '7', 'total md calculated');
+    assert.equal(this.element.querySelector('[data-test-total-pm]').textContent, '11', 'total pm calculated');
+    assert.equal(this.element.querySelector('[data-test-total-saturday]').textContent, '15', 'total saturday calculated');
   });
 });
