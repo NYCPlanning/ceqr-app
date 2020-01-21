21535ec0e17ac06a31cd247380f9a5fbd1b71e74
Matt Gardner <wilburnforce@gmail.com>
2020-01-21 11:37:27 -0500

    Remove unused data
    I was thrown off by the amount of unused & stale data in this fixture. Only certain properties are used so I removed everything else.

--- frontend/mirage/fixtures/carto-map.js
+++ frontend/mirage/fixtures/carto-map.js
@@ -1,22 +1,9 @@
 export default {
-  layergroupid: '7d6af89fdf6955ba373c070155f60b4c:1569945388239',
   metadata: {
-    layers: [{
-      type: 'mapnik', id: 'ember375', meta: { stats: { estimatedFeatureCount: 857321 }, aggregation: { png: false, mvt: false } }, tilejson: { vector: { tilejson: '2.2.0', tiles: ['https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt'] } },
-    }, {
-      type: 'mapnik', id: 'ember376', meta: { stats: { estimatedFeatureCount: 857321 }, aggregation: { png: false, mvt: false } }, tilejson: { vector: { tilejson: '2.2.0', tiles: ['https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt'] } },
-    }, {
-      type: 'mapnik', id: 'ember377', meta: { stats: { estimatedFeatureCount: 857321 }, aggregation: { png: false, mvt: false } }, tilejson: { vector: { tilejson: '2.2.0', tiles: ['https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt'] } },
-    }, {
-      type: 'mapnik', id: 'ember378', meta: { stats: { estimatedFeatureCount: 857321 }, aggregation: { png: false, mvt: false } }, tilejson: { vector: { tilejson: '2.2.0', tiles: ['https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt'] } },
-    }, {
-      type: 'mapnik', id: 'ember379', meta: { stats: { estimatedFeatureCount: 857321 }, aggregation: { png: false, mvt: false } }, tilejson: { vector: { tilejson: '2.2.0', tiles: ['https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt'] } },
-    }],
-    dataviews: {},
-    analyses: [],
-    tilejson: { vector: { tilejson: '2.2.0', tiles: ['https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt'] } },
-    url: { vector: { urlTemplate: 'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt', subdomains: ['a', 'b', 'c', 'd'] } },
+    tilejson: {
+      vector: {
+        tiles: ['https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/dummy-tile.mvt'],
+      },
+    },
   },
-  cdn_url: { templates: { http: { subdomains: ['0', '1', '2', '3'], url: 'http://{s}.gusc.cartocdn.com' }, https: { subdomains: ['a', 'b', 'c', 'd'], url: 'https://cartocdn-gusc-{s}.global.ssl.fastly.net' } }, http: 'gusc.cartocdn.com', https: 'cartocdn-gusc.global.ssl.fastly.net' },
-  last_updated: '2019-10-01T15:56:28.239Z',
 };
