c7126c9a24c8e68ca10a862567b8e5bb6d0536e7
Matt Gardner <wilburnforce@gmail.com>
2020-01-21 12:24:56 -0500

    Add mock route for mapbox style request
    I stubbed in the fetch request for the mapbox style that’s hosted on planninglabs so that our test suite has fewer environmental variables.

---
+++ frontend/mirage/fixtures/dummy-mapbox-style.js
@@ -0,0 +1,18 @@
+export default {
+  version: 8,
+  name: 'NYCPlanning Positron',
+  metadata: {
+    attribution: 'Based on OpenMapTiles Positron style: https://github.com/openmaptiles/positron-gl-style',
+  },
+  center: [
+    -73.869324,
+    40.815888,
+  ],
+  zoom: 9.72,
+  bearing: 0,
+  pitch: 0,
+  sources: {},
+  sprite: '',
+  glyphs: 'https://tiles.planninglabs.nyc/fonts/{fontstack}/{range}.pbf',
+  layers: [],
+};
