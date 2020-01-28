a96f37fd1ad2dc87571a56b4590ae6ee858722e9
Matt Gardner <wilburnforce@gmail.com>
2020-01-28 12:14:45 -0500

    Add basic rendering tests to nudge test coverage

---
+++ frontend/tests/integration/components/public-schools/summary-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Integration | Component | public-schools/summary', function(hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  test('it renders', async function(assert) {
+    const store = this.owner.lookup('service:store');
+
+    this.server.create('public-schools-analysis', { id: 1 });
+    this.analysis = await store.findRecord('public-schools-analysis', 1);
+
+    await render(hbs`
+      <PublicSchools::Summary
+        @analysis={{this.analysis}}
+      />
+    `);
+
+    assert.ok(this.element);
+  });
+});
