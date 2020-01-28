b0fb0b11a760645ba0aa116b3aa90bcd8fbd5715
Matt Gardner <wilburnforce@gmail.com>
2020-01-28 12:13:48 -0500

    Add deprecation workflow addon for quieter console
    After the upgrade, many new APIs were deprecated. This meant that we now saw 800 new deprecations, most of which were from addons. Some are from source, however.
    
    This commit adds an addon that gathers the ids of deprecations and silences them. This is explicitly handled in a config file.

---
+++ frontend/config/deprecation-workflow.js
@@ -0,0 +1,9 @@
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: 'silence', matchId: 'ember-views.curly-components.jquery-element' },
+    { handler: 'silence', matchId: 'ember-data:record-lifecycle-event-methods' },
+    { handler: 'silence', matchId: 'computed-property.override' }
+  ]
+};
