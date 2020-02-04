53a579f7bbc13627c724b6dabb37e896811a20e0
trbmcginnis <taylormcgin@gmail.com>
2020-02-04 15:39:03 -0500

    630: create computed for newlyOpenedSchools and lcgms metadata to replace old lcgms array in recently-built-schools

--- frontend/app/components/public-schools/existing-conditions/recently-built-schools.js
+++ frontend/app/components/public-schools/existing-conditions/recently-built-schools.js
@@ -1,6 +1,15 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  newlyOpenedSchools: computed('analysis.ceqr_school_buildings', function() {
+    return this.analysis.ceqr_school_buildings.find((school) => school.source === 'lcgms');
+  }),
+
+  doeLcgmsMetadata: computed('analysis.dataPackage.schemas.ceqr_school_buildings.sources', function() {
+    return this.analysis.dataPackage.schemas.ceqr_school_buildings.sources.find((source) => source.name === 'lcgms');
+  }),
+
   actions: {
     save() {
       this.set('saving', true);
