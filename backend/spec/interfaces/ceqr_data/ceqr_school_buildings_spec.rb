905db2673669b46164ea826431f4e0fdf96bced6
trbmcginnis <taylormcgin@gmail.com>
2020-02-05 17:02:32 -0500

    629: ceqr_school_buildings Data Update: interface spec
    Recent data updates combined lcgms and bluebook datasets into one
    dataset called ceqr_school_buildings. This commit removes the lcgms
    and bluebook interface specs and adds one for ceqr_school_buildings
    interface.

--- backend/spec/interfaces/ceqr_data/ceqr_school_buildings_spec.rb
+++ backend/spec/interfaces/ceqr_data/ceqr_school_buildings_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "CeqrData Sca Bluebook", type: :model do
-  ### BLUEBOOK SCHOOLS
-  context "Bluebook Schools" do
-    let(:sca_bluebook_2018) { CeqrData::ScaBluebook.version('2018') }
-    let(:sca_bluebook_2017) { CeqrData::ScaBluebook.version('2017') }
+RSpec.describe "CeqrData Ceqr School Buildings", type: :model do
+  ### CEQR_SCHOOL_BUILDINGS
+  context "ceqr_school_buildings" do
+    let(:ceqr_school_buildings_2019) { CeqrData::CeqrSchoolBuildings.version('2019') }
+    let(:ceqr_school_buildings_2018) { CeqrData::CeqrSchoolBuildings.version('2018') }
+    let(:ceqr_school_buildings_2017) { CeqrData::CeqrSchoolBuildings.version('2017') }
 
-    ### PRIMARY SCHOOL bluebook_schools VERSION 2018
-    it "returns an array of bluebooks that match subdistrict VERSION 2018" do
+    ### PRIMARY SCHOOL ceqr_school_buildings VERSION 2019
+    it "returns an array of ceqr_school_buildings that match subdistrict VERSION 2019" do
       subdistricts = ["(2,1)"]
 
-      ps_schools = sca_bluebook_2018.ps_schools_in_subdistricts(subdistricts)
+      ps_schools = ceqr_school_buildings_2019.ps_schools_in_subdistricts(subdistricts)
 
       expect(ps_schools.first[:district]).to be_an Integer
       expect(ps_schools.first[:subdistrict]).to be_an Integer
@@ -35,11 +36,11 @@ RSpec.describe "CeqrData Sca Bluebook", type: :model do
       expect(ps_schools.first[:subdistrict]).to eq(1)
     end
 
-    ### PRIMARY SCHOOL bluebook_schools VERSION 2017
-    it "returns an array of bluebooks that match subdistrict VERSION 2017" do
+    ### PRIMARY SCHOOL ceqr_school_buildings VERSION 2018
+    it "returns an array of ceqr_school_buildings that match subdistrict VERSION 2018" do
       subdistricts = ["(2,1)"]
 
-      ps_schools = sca_bluebook_2017.ps_schools_in_subdistricts(subdistricts)
+      ps_schools = ceqr_school_buildings_2018.ps_schools_in_subdistricts(subdistricts)
 
       expect(ps_schools.first[:district]).to be_an Integer
       expect(ps_schools.first[:subdistrict]).to be_an Integer
@@ -64,11 +65,40 @@ RSpec.describe "CeqrData Sca Bluebook", type: :model do
       expect(ps_schools.first[:subdistrict]).to eq(1)
     end
 
-    ### INTERMEDIATE SCHOOL bluebook_schools VERSION 2018
-    it "returns an array of bluebooks that match subdistrict" do
+    ### PRIMARY SCHOOL ceqr_school_buildings VERSION 2017
+    it "returns an array of ceqr_school_buildings that match subdistrict VERSION 2017" do
       subdistricts = ["(2,1)"]
 
-      is_schools = sca_bluebook_2018.is_schools_in_subdistricts(subdistricts)
+      ps_schools = ceqr_school_buildings_2017.ps_schools_in_subdistricts(subdistricts)
+
+      expect(ps_schools.first[:district]).to be_an Integer
+      expect(ps_schools.first[:subdistrict]).to be_an Integer
+      expect(ps_schools.first[:borocode]).to be_an Integer
+
+      expect(ps_schools.first[:ps_capacity]).to be_an Integer
+      expect(ps_schools.first[:ps_enroll]).to be_an Integer
+      expect(ps_schools.first[:ms_capacity]).to be_an Integer
+      expect(ps_schools.first[:ms_enroll]).to be_an Integer
+      expect(ps_schools.first[:hs_capacity]).to be_an Integer
+      expect(ps_schools.first[:hs_enroll]).to be_an Integer
+
+      expect(ps_schools.first[:bldg_name]).to be_a String
+      expect(ps_schools.first[:bldg_id]).to be_a String
+      expect(ps_schools.first[:org_id]).to be_a String
+      expect(ps_schools.first[:org_level]).to be_a String
+      expect(ps_schools.first[:address]).to be_a String
+
+      expect(ps_schools.first[:excluded]).to be_in([true, false])
+
+      expect(ps_schools.first[:district]).to eq(2)
+      expect(ps_schools.first[:subdistrict]).to eq(1)
+    end
+
+    ### INTERMEDIATE SCHOOL ceqr_school_buildings VERSION 2019
+    it "returns an array of intermediate ceqr_school_buildings that match subdistrict for VERSION 2019" do
+      subdistricts = ["(2,1)"]
+
+      is_schools = ceqr_school_buildings_2019.is_schools_in_subdistricts(subdistricts)
 
       expect(is_schools.first[:district]).to be_an Integer
       expect(is_schools.first[:subdistrict]).to be_an Integer
@@ -93,11 +123,11 @@ RSpec.describe "CeqrData Sca Bluebook", type: :model do
       expect(is_schools.first[:subdistrict]).to eq(1)
     end
 
-    ### INTERMEDIATE SCHOOL bluebook_schools VERSION 2017
-    it "returns an array of bluebooks that match subdistrict" do
+    ### INTERMEDIATE SCHOOL ceqr_school_buildings VERSION 2018
+    it "returns an array of intermediate ceqr_school_buildings that match subdistrict for VERSION 2018" do
       subdistricts = ["(2,1)"]
 
-      is_schools = sca_bluebook_2017.is_schools_in_subdistricts(subdistricts)
+      is_schools = ceqr_school_buildings_2018.is_schools_in_subdistricts(subdistricts)
 
       expect(is_schools.first[:district]).to be_an Integer
       expect(is_schools.first[:subdistrict]).to be_an Integer
@@ -122,11 +152,68 @@ RSpec.describe "CeqrData Sca Bluebook", type: :model do
       expect(is_schools.first[:subdistrict]).to eq(1)
     end
 
-    ### HIGH SCHOOL bluebook_schools VERSION 2018
-    it "returns an array of bluebooks that match borough" do
+    ### INTERMEDIATE SCHOOL ceqr_school_buildings VERSION 2017
+    it "returns an array of intermediate ceqr_school_buildings that match subdistrict for VERSION 2017" do
+      subdistricts = ["(2,1)"]
+
+      is_schools = ceqr_school_buildings_2017.is_schools_in_subdistricts(subdistricts)
+
+      expect(is_schools.first[:district]).to be_an Integer
+      expect(is_schools.first[:subdistrict]).to be_an Integer
+      expect(is_schools.first[:borocode]).to be_an Integer
+
+      expect(is_schools.first[:ps_capacity]).to be_an Integer
+      expect(is_schools.first[:ps_enroll]).to be_an Integer
+      expect(is_schools.first[:ms_capacity]).to be_an Integer
+      expect(is_schools.first[:ms_enroll]).to be_an Integer
+      expect(is_schools.first[:hs_capacity]).to be_an Integer
+      expect(is_schools.first[:hs_enroll]).to be_an Integer
+
+      expect(is_schools.first[:bldg_name]).to be_a String
+      expect(is_schools.first[:bldg_id]).to be_a String
+      expect(is_schools.first[:org_id]).to be_a String
+      expect(is_schools.first[:org_level]).to be_a String
+      expect(is_schools.first[:address]).to be_a String
+
+      expect(is_schools.first[:excluded]).to be_in([true, false])
+
+      expect(is_schools.first[:district]).to eq(2)
+      expect(is_schools.first[:subdistrict]).to eq(1)
+    end
+
+    ### HIGH SCHOOL ceqr_school_buildings VERSION 2019
+    it "returns an array of high school ceqr_school_buildings that match borough for VERSION 2019" do
       boroIntegers = [1, 2]
 
-      hs_schools = sca_bluebook_2018.high_schools_in_boro(boroIntegers)
+      hs_schools = ceqr_school_buildings_2019.high_schools_in_boro(boroIntegers)
+
+      expect(hs_schools.first[:district]).to be_an Integer
+      expect(hs_schools.first[:subdistrict]).to be_an Integer
+      expect(hs_schools.first[:borocode]).to be_an Integer
+
+      expect(hs_schools.first[:ps_capacity]).to be_an Integer
+      expect(hs_schools.first[:ps_enroll]).to be_an Integer
+      expect(hs_schools.first[:ms_capacity]).to be_an Integer
+      expect(hs_schools.first[:ms_enroll]).to be_an Integer
+      expect(hs_schools.first[:hs_capacity]).to be_an Integer
+      expect(hs_schools.first[:hs_enroll]).to be_an Integer
+
+      expect(hs_schools.first[:bldg_name]).to be_a String
+      expect(hs_schools.first[:bldg_id]).to be_a String
+      expect(hs_schools.first[:org_id]).to be_a String
+      expect(hs_schools.first[:org_level]).to be_a String
+      expect(hs_schools.first[:address]).to be_a String
+
+      hs_schools_unique_borocodes = hs_schools.map {|x| x[:borocode]}.uniq.sort! # [1, 2]
+
+      expect(hs_schools_unique_borocodes).to eq([1, 2])
+    end
+
+    ### HIGH SCHOOL ceqr_school_buildings VERSION 2018
+    it "returns an array of high school ceqr_school_buildings that match borough for VERSION 2018" do
+      boroIntegers = [1, 2]
+
+      hs_schools = ceqr_school_buildings_2018.high_schools_in_boro(boroIntegers)
 
       expect(hs_schools.first[:district]).to be_an Integer
       expect(hs_schools.first[:subdistrict]).to be_an Integer
@@ -151,11 +238,11 @@ RSpec.describe "CeqrData Sca Bluebook", type: :model do
     end
 
 
-    ### HIGH SCHOOL bluebook_schools VERSION 2017
-    it "returns an array of bluebooks that match borough" do
+    ### HIGH SCHOOL ceqr_school_buildings VERSION 2017
+    it "returns an array of high school ceqr_school_buildings that match borough for VERSION 2017" do
       boroIntegers = [1, 2]
 
-      hs_schools = sca_bluebook_2017.high_schools_in_boro(boroIntegers)
+      hs_schools = ceqr_school_buildings_2017.high_schools_in_boro(boroIntegers)
 
       expect(hs_schools.first[:district]).to be_an Integer
       expect(hs_schools.first[:subdistrict]).to be_an Integer
