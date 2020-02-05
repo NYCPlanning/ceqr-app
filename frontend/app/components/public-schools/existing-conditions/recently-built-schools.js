366bd6b18dbe3beb239d142eb218451157d871dc
trbmcginnis <taylormcgin@gmail.com>
2020-02-05 13:31:09 -0500

    630: ceqr_school_buildings Data Update: recently-built-schools lcgms schools & metadata
    Recent data updates combined lcgms and bluebook dataset into one
    dataset called ceqr_school_buildings. This commit adds two new
    computeds to recently-built-schools component that reflect newly
    opened schools (lcgms schools) and lcgms metadata (this data update also
    included an update in the data package that holds the metadata).

--- frontend/app/components/public-schools/existing-conditions/recently-built-schools.js
+++ frontend/app/components/public-schools/existing-conditions/recently-built-schools.js
@@ -1,6 +1,18 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  // ceqr_school_buildings dataset is a combination of two datasets lcgms and bluebook
+  // lcgms dataset represents schools that opened recently
+  newlyOpenedSchools: computed('analysis.ceqr_school_buildings', function() {
+    return this.analysis.ceqr_school_buildings.find((school) => school.source === 'lcgms');
+  }),
+
+  // ceqr_school_buildings dataset is a combination of two datasets with different metadata: lcgms dataset and bluebook dataset
+  lcgmsMetadata: computed('analysis.dataPackage.schemas.ceqr_school_buildings.sources', function() {
+    return this.analysis.dataPackage.schemas.ceqr_school_buildings.sources.find((source) => source.name === 'lcgms');
+  }),
+
   actions: {
     save() {
       this.set('saving', true);
