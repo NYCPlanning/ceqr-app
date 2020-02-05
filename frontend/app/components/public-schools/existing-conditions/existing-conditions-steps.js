e6e65e8bcaf760e5d55aee6a4f7adc520e2f65e7
trbmcginnis <taylormcgin@gmail.com>
2020-02-05 12:19:00 -0500

    630: ceqr_school_buildings Data Update: existing-conditions-steps lcgms schools
    Recent data updates combined bluebook and lcgms datasets into one
    dataset called ceqr_school_buildings. Create new computed to represent
    newly opened schools by filtering for lcgms in ceqr_school_buildings.

--- frontend/app/components/public-schools/existing-conditions/existing-conditions-steps.js
+++ frontend/app/components/public-schools/existing-conditions/existing-conditions-steps.js
@@ -2,6 +2,12 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  // ceqr_school_buildings dataset is a combination of lcgms and bluebook datasets
+  // lcgms dataset represents schools that were opened recently
+  newlyOpenedSchools: computed('analysis.ceqr_school_buildings', function() {
+    return this.analysis.ceqr_school_buildings.find((school) => school.source === 'lcgms');
+  }),
+
   displayWarning: computed('analysis.{esSchoolChoice,isSchoolChoice,subdistrictsFromDb}', function() {
     return (
       this.analysis.esSchoolChoice
