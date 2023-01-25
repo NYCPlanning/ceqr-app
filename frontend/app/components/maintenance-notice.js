e60e23807b72ac8c695478b4d8327c8a94f25466
horatio <horatiorosa@aol.com>
2023-01-25 17:01:11 -0500

    12070 added maintenance banner to ceqr login page

---
+++ frontend/app/components/maintenance-notice.js
@@ -0,0 +1,44 @@
+import Component from '@glimmer/component';
+import ENV from 'labs-ceqr/config/environment';
+
+const MAINTENANCE_RANGE = ENV.maintenanceTimes;
+const [start, end] = MAINTENANCE_RANGE
+  .map((string) => new Date(string));
+
+const userReadableTime = function(time) {
+  return time.toLocaleDateString('en-us',
+    {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+};
+
+export default class MaintenanceNoticeComponent extends Component {
+  get maintenanceStart() {
+    const maintenanceStart = userReadableTime(start);
+
+    return maintenanceStart;
+  }
+
+  get maintenanceEnd() {
+    const maintenanceEnd = userReadableTime(end);
+
+    return maintenanceEnd;
+  }
+
+  get hasUpcomingMaintenance() {
+    const now = new Date();
+
+    return now < start;
+  }
+
+  get isMaintenancePeriod() {
+    const now = new Date();
+
+    return now > start && now < end;
+  }
+}
