3cc246948536e3004aff8505bc469f88e0a70a4f
tangoyankee <miller.tim108@gmail.com>
2023-07-10 10:23:24 -0400

    list all years in options

--- backend/db/migrate/20230124164916_fix2020_data_package_spelling.rb
+++ backend/db/migrate/20230124164916_fix2020_data_package_spelling.rb
@@ -1,17 +1,17 @@
 class Fix2020DataPackageSpelling < ActiveRecord::Migration[5.2]
-  def up
-    dataPackage = DataPackage.find_by(name: 'Februrary 2020')
+  # def up
+  #   dataPackage = DataPackage.find_by(name: 'Februrary 2020')
 
-    dataPackage.update_attributes({
-      name: 'February 2020'
-    })
-  end
+  #   dataPackage.update_attributes({
+  #     name: 'February 2020'
+  #   })
+  # end
 
-  def down
-    dataPackage = DataPackage.find_by(name: 'February 2020')
+  # def down
+  #   dataPackage = DataPackage.find_by(name: 'February 2020')
 
-    dataPackage.update_attributes({
-      name: 'Februrary 2020'
-    })
-  end
+  #   dataPackage.update_attributes({
+  #     name: 'Februrary 2020'
+  #   })
+  # end
 end
