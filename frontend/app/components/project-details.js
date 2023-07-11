59fccd27c6a982202530548889962dd6e3222ec4
tangoyankee <miller.tim108@gmail.com>
2023-07-11 15:48:29 -0400

    attempt to use select tag

--- frontend/app/components/project-details.js
+++ frontend/app/components/project-details.js
@@ -3,8 +3,16 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   'project-orchestrator': service(),
+  yearSelection: null,
 
   buildYearRange: Array.from({ length: (2040 - 2018) }, (v, k) => k + 2018),
+
+  init() {
+    this._super(...arguments);
+    console.info('arguments', ...arguments);
+    const buildYear = this.get('buildYear');
+    console.info('init buildYear', buildYear);
+  },
 
   // noop
   save() { },
@@ -17,5 +25,19 @@ export default Component.extend({
     back() {
       history.back();
     },
+    changeBuildYear() {
+      console.info('changing build year');
+      const changeAction = this.get('action');
+      console.info('changeAction', changeAction);
+      const selectedEl = this.$('select')[0];
+      const selectedIndex = selectedEl.selectedIndex;
+      const buildYearRange = this.get('buildYearRange');
+      const selectedValue = buildYearRange[selectedIndex];
+      const project = this.get('project', 'buildYear');
+      console.info('buildYear', project);
+      console.info('project', this.project.buildYear);
+      this.project.buildYear = selectedValue;
+      console.info('selectedValue', selectedValue);
+    }
   },
 });
