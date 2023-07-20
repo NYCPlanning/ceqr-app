4fdbb35b44e2c7557f4df614dc0928af6d9caaaa
tangoyankee <miller.tim108@gmail.com>
2023-07-20 11:33:06 -0400

    lint current user service

--- frontend/app/services/current-user.js
+++ frontend/app/services/current-user.js
@@ -1,20 +1,21 @@
 import Service, { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 import { isEmpty } from '@ember/utils';
 import RSVP from 'rsvp';
 
-export default Service.extend({
-  session: service(),
-  store: service(),
+export default class CurrentUserService extends Service {
+  @service session;
+  @service store;
 
   load() {
-    const userId = this.get('session.data.authenticated.tokenData.user_id');
+    const userId = this.session?.data?.authenticated?.tokenData?.user_id;
 
     if (!isEmpty(userId)) {
       return this.store.findRecord('user', userId).then((user) => {
-        this.set('user', user);
+        set(this, 'user', user);
       });
     }
     return RSVP.resolve();
-  },
-});
+  }
+}
