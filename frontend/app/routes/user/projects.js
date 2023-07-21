9b11106aee92d526c435878cb2cc2b0541e57e1a
tangoyankee <miller.tim108@gmail.com>
2023-07-21 11:43:48 -0400

    lint user routes

--- frontend/app/routes/user/projects.js
+++ frontend/app/routes/user/projects.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import $ from 'jquery';
+import { action, set } from '@ember/object';
 
-export default Route.extend({
-  currentUser: service(),
+export default class UserProjectsRoute extends Route {
+  @service currentUser;
 
   async model() {
     return this.store
@@ -11,26 +12,27 @@ export default Route.extend({
       .then(function (projects) {
         return projects.filterBy('isNew', false);
       });
-  },
+  }
 
   afterModel(model) {
     // if no projects are found, reroute them to the intro page
     if (model.length < 1) {
       this.transitionTo('ceqr-intro-page');
     }
-  },
+  }
 
-  actions: {
-    deleteModal(id) {
-      this.set('deleteProjectId', id);
-      $('.mini.modal').modal('show');
-    },
-    deleteProject() {
-      const id = this.deleteProjectId;
-      this.store
-        .findRecord('project', id, { backgroundReload: false })
-        .then((p) => p.destroyRecord())
-        .then(() => this.refresh());
-    },
-  },
-});
+  @action
+  deleteModal(id) {
+    set(this, 'deleteProjectId', id);
+    $('.mini.modal').modal('show');
+  }
+
+  @action
+  deleteProject() {
+    const id = this.deleteProjectId;
+    this.store
+      .findRecord('project', id, { backgroundReload: false })
+      .then((p) => p.destroyRecord())
+      .then(() => this.refresh());
+  }
+}
