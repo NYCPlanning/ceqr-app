908a73959e68594b55105db61cdc2bdd3fbc8928
tangoyankee <miller.tim108@gmail.com>
2023-07-27 10:21:28 -0400

    lint adapters

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,31 +1,28 @@
-import DS from 'ember-data';
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
 import ENV from 'labs-ceqr/config/environment';
 
-export default DS.JSONAPIAdapter.extend({
-  session: service(),
-  host: ENV.host,
-  namespace: 'api/v1',
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  @service() session;
+  host = ENV.host;
+  namespace = 'api/v1';
 
-  headers: computed(
-    'session.isAuthenticated',
-    'session.data.authenticated.token',
-    function () {
-      if (this.session.isAuthenticated) {
-        return {
-          Authorization: `Bearer ${this.session.data.authenticated.token}`,
-        };
-      }
-      return {};
+  @computed('session.{isAuthenticated,data.authenticated.token}', function () {
+    if (this.session.isAuthenticated) {
+      return {
+        Authorization: `Bearer ${this.session.data.authenticated.token}`,
+      };
     }
-  ),
+    return {};
+  })
+  headers;
 
   handleResponse(status) {
     if (status === 401 && this.session.isAuthenticated) {
       this.session.invalidate();
     }
-    return this._super(...arguments);
-  },
-});
+    return super.handleResponse(...arguments);
+  }
+}
