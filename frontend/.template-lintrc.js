9e512eaf32cd4b776656573c8e97d5f412e240ec
tangoyankee <miller.tim108@gmail.com>
2023-07-31 14:46:21 -0400

    demote general template lint rules to warnings

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -5,5 +5,14 @@ module.exports = {
   extends: 'recommended',
   rules: {
     'no-inline-styles': 'warn',
+    'no-action': 'warn',
+    'no-implicit-this': 'warn',
+    'no-curly-component-invocation': 'warn',
+    'no-yield-only': 'warn',
+    'no-invalid-interactive': 'warn',
+    'require-input-label': 'warn',
+    'no-link-to-positional-params': 'warn',
+    'link-href-attributes': 'warn',
+    'no-duplicate-id': 'warn'
   },
 };
