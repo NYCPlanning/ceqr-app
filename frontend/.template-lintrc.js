62be1902938f7cf873d1d962363ceeb06e6e175c
tangoyankee <miller.tim108@gmail.com>
2023-08-03 15:22:12 -0400

    reinstate no-link-to-positional-params rule

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     'no-inline-styles': 'warn',
     'no-action': 'warn',
     'no-invalid-interactive': 'warn',
-    'no-link-to-positional-params': 'warn',
     'link-href-attributes': 'warn',
     'no-duplicate-id': 'warn',
   },
