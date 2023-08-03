552fd4f3c979e798c8f8a8234f3b7335faf863b1
tangoyankee <miller.tim108@gmail.com>
2023-08-03 14:57:16 -0400

    add labels to form inputs

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     'no-inline-styles': 'warn',
     'no-action': 'warn',
     'no-invalid-interactive': 'warn',
-    'require-input-label': 'warn',
     'no-link-to-positional-params': 'warn',
     'link-href-attributes': 'warn',
     'no-duplicate-id': 'warn',
